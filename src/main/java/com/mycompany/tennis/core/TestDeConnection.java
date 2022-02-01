a9996061a2725de563425f27f0feca3c4ba8696c
diallo <souleymane.diallo@etudiant-univ-rennes1.fr>
2022-02-01 12:54:40 +0100

    ECrire dans la base de données

--- src/main/java/com/mycompany/tennis/core/TestDeConnection.java
+++ src/main/java/com/mycompany/tennis/core/TestDeConnection.java
@@ -15,36 +15,30 @@ public class TestDeConnection {
             Etape 1 : création d'un objet de type statement
             on passe la requête immédiatement dans la création du prepareStatement
             car il a l'avantage de précompiler la requête
-
              */
-            PreparedStatement preparedStatement = conn.prepareStatement("SELECT NOM,PRENOM,ID FROM JOUEUR WHERE ID=?");
+
+            /*
+            on veut modifier les paramètres d'un joueur via UPDATE
+             */
+
+            PreparedStatement preparedStatement = conn.prepareStatement("UPDATE JOUEUR SET NOM=?,PRENOM=? WHERE ID=?");
             /*
             les méthodes set de preparedStatement prennent en 1er paramètre
             l'index du ? dans la requête sql,
             ici, on a un seul paramètre,
             le 2ème paramètre est la valeur qu'on veut donner à ce ?
              */
-            long identifiant=39L;
-            preparedStatement.setLong(1,identifiant);
+            long identifiant=24L;
+            String nom="Errani";
+            String prenom="Sara";
+            preparedStatement.setString(1,nom);
+            preparedStatement.setString(2,prenom);
+            preparedStatement.setLong(3,identifiant);
 
 
-            ResultSet rs=preparedStatement.executeQuery();
-
-            /*
-            On veut récupérer la colonne nom, prenom, d'où l'utilisation du while
-             */
-
-            if(rs.next()){
-                final String nom=rs.getString("NOM");
-                final String prenom=rs.getString("PRENOM");
-                final Long id=rs.getLong("ID");
-                System.out.println("Le joueur / la joueuse représenté(e) par le numéro "+id+" est "+prenom+" "+nom);
-            }
-            else {
-                System.out.println("Il n'y a pas d'enregistrement d'ID 39");
-            }
-
-
+            int nbEnregistrementsModifies=preparedStatement.executeUpdate();
+            System.out.println("le nombre d'enregistrement modifié est "+nbEnregistrementsModifies);
+            
             System.out.println("success");
         } catch (SQLException e) {
             e.printStackTrace();
