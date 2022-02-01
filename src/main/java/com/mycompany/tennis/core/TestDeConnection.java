4ddb24ba492808e8d1f34c350c0a938dee90a24f
diallo <souleymane.diallo@etudiant-univ-rennes1.fr>
2022-02-01 12:25:39 +0100

    Requêtes paramétrées : PreparedStatement

--- src/main/java/com/mycompany/tennis/core/TestDeConnection.java
+++ src/main/java/com/mycompany/tennis/core/TestDeConnection.java
@@ -13,10 +13,22 @@ public class TestDeConnection {
             conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/TENNIS?useSSL=false","root","my-secret-pw");
             /*
             Etape 1 : création d'un objet de type statement
+            on passe la requête immédiatement dans la création du prepareStatement
+            car il a l'avantage de précompiler la requête
 
              */
-            Statement statement = conn.createStatement();
-            ResultSet rs=statement.executeQuery("SELECT NOM,PRENOM,ID FROM JOUEUR WHERE ID=12 ");
+            PreparedStatement preparedStatement = conn.prepareStatement("SELECT NOM,PRENOM,ID FROM JOUEUR WHERE ID=?");
+            /*
+            les méthodes set de preparedStatement prennent en 1er paramètre
+            l'index du ? dans la requête sql,
+            ici, on a un seul paramètre,
+            le 2ème paramètre est la valeur qu'on veut donner à ce ?
+             */
+            long identifiant=39L;
+            preparedStatement.setLong(1,identifiant);
+
+
+            ResultSet rs=preparedStatement.executeQuery();
 
             /*
             On veut récupérer la colonne nom, prenom, d'où l'utilisation du while
@@ -29,7 +41,7 @@ public class TestDeConnection {
                 System.out.println("Le joueur / la joueuse représenté(e) par le numéro "+id+" est "+prenom+" "+nom);
             }
             else {
-                System.out.println("Il n'y a pas d'enregositrement d'ID 12");
+                System.out.println("Il n'y a pas d'enregistrement d'ID 39");
             }
 
 
