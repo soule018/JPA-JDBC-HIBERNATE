96d326787892c2dca6be682e709ba561be999657
diallo <souleymane.diallo@etudiant-univ-rennes1.fr>
2022-02-01 13:36:27 +0100

    La transaction (Commit / rollback)

--- src/main/java/com/mycompany/tennis/core/TestDeConnection.java
+++ src/main/java/com/mycompany/tennis/core/TestDeConnection.java
@@ -6,11 +6,19 @@ public class TestDeConnection {
     public static void main(String... args){
         Connection conn = null;
         try {
-            //Seulement avant Java 7/JDBC 4
-            //Class.forName(DRIVER_CLASS_NAME);
 
-            //MySQL driver MySQL Connector
             conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/TENNIS?useSSL=false","root","my-secret-pw");
+
+            /*
+            Toutes les modifications que nous allons faire vont être mise
+            en attente de validation/de commit et c'est nous qui allons
+            réaliser la validation en écrivant conn.commit() si tout est bon;
+            Si tout ne se passe pas bien, on aura une execption, qui va nous
+            emmener dans le bloc catch(SQLException e), donc on va demander
+            d'annuler tout en écrivant coll.rollback(); qui veut dire revient en arrière
+            Le rollback peut lui-même rejeter une exception, il faudrait encadrer cette instruction d'un try catch également
+             */
+            conn.setAutoCommit(false);
             /*
             Etape 1 : création d'un objet de type statement
             on passe la requête immédiatement dans la création du prepareStatement
@@ -21,27 +29,52 @@ public class TestDeConnection {
             on veut modifier les paramètres d'un joueur via UPDATE
              */
 
-            PreparedStatement preparedStatement = conn.prepareStatement("UPDATE JOUEUR SET NOM=?,PRENOM=? WHERE ID=?");
+            PreparedStatement preparedStatement = conn.prepareStatement("INSERT INTO JOUEUR (NOM,PRENOM,SEXE) VALUES (?,?,?)");
             /*
             les méthodes set de preparedStatement prennent en 1er paramètre
             l'index du ? dans la requête sql,
             ici, on a un seul paramètre,
             le 2ème paramètre est la valeur qu'on veut donner à ce ?
              */
-            long identifiant=24L;
-            String nom="Errani";
-            String prenom="Sara";
+
+            String nom="Capriati";
+            String prenom="Jennifer";
+            String sexe="F";
             preparedStatement.setString(1,nom);
             preparedStatement.setString(2,prenom);
-            preparedStatement.setLong(3,identifiant);
+            preparedStatement.setString(3,sexe);
+
+            preparedStatement.executeUpdate();
+
+             nom="Johannson";
+             prenom="Thomas";
+             sexe="H";
+            preparedStatement.setString(1,nom);
+            preparedStatement.setString(2,prenom);
+            preparedStatement.setString(3,sexe);
+
+            preparedStatement.executeUpdate();
+
+            /*
+            C'est à l'issu seulement de la 2ème éxécution, si tout
+            s'est bien passé, que je vais tenter de faire la validation
+            à savoir conn.commit()
+            Si la 1ère instruction se passe mal,j'irai dans le bloc
+            try catch;
+            Si la 1ère a fonctionné et que la 2ème échoue, je vais tomber
+            dans le bloc SQLException e qui devrait annuler la 1ère instruction également
+             */
+            conn.commit();
 
 
-            int nbEnregistrementsModifies=preparedStatement.executeUpdate();
-            System.out.println("le nombre d'enregistrement modifié est "+nbEnregistrementsModifies);
-            
             System.out.println("success");
         } catch (SQLException e) {
             e.printStackTrace();
+            try {
+                if(conn!=null) conn.rollback();
+            } catch (SQLException e1) {
+                e1.printStackTrace();
+            }
         }
         finally {
             try {
