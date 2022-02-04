ea8d2a697aeced7d698c94b9bfd530aa835a91d0
diallo <souleymane.diallo@etudiant-univ-rennes1.fr>
2022-02-04 10:42:11 +0100

    Exercice 1 / invocation de la méthode list()

--- src/main/java/com/mycompany/tennis/core/TestDeConnection.java
+++ src/main/java/com/mycompany/tennis/core/TestDeConnection.java
@@ -2,42 +2,20 @@ package com.mycompany.tennis.core;
 
 import com.mycompany.tennis.core.entity.Tournoi;
 import com.mycompany.tennis.core.repository.TournoiRepositoryImpl;
-import com.mysql.cj.jdbc.MysqlDataSource;
-import org.apache.commons.dbcp2.BasicDataSource;
-
-import java.sql.*;
 import java.util.List;
 
 public class TestDeConnection {
     public static void main(String... args){
         TournoiRepositoryImpl tournoiRepository=new TournoiRepositoryImpl();
 
-        tournoiRepository.delete(18L);
+        List<Tournoi>listTournoi=tournoiRepository.list();
+
+        for(Tournoi list : listTournoi){
+            System.out.println(list.getNom()+" "+list.getCode());
+        }
 
 
 
-
-
-
-       // Joueur noah =joueurRepository.getById(70L);
-        //System.out.println(noah.getPrenom()+" "+noah.getNom());
-       // noah.setPrenom("Yannick");
-        //joueurRepository.update(noah);
-        //System.out.println(noah.getPrenom()+" "+noah.getNom());
-       /*
-        List<Joueur>listJoueur =joueurRepository.list();
-        Joueur noah=new Joueur();
-        noah.setNom("Noah");
-        noah.setPrenom("Yannik");
-        noah.setSexe('H');
-        joueurRepository.create(noah);
-
-        System.out.println("L'identifiant du joueur crée est "+noah.getId());
-
-
-        for(Joueur joueur : listJoueur){
-            System.out.println(joueur.getPrenom()+" "+joueur.getNom());
-        }*/
 
 
 
