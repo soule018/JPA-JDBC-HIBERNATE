2d22224eb6dcc026f4adfb8e9eec38e0aac409bf
diallo <souleymane.diallo@etudiant-univ-rennes1.fr>
2022-02-08 17:23:42 +0100

    Section 7 / Exclure une propriété avec @Transient

--- src/main/java/com/mycompany/tennis/core/entity/Epreuve.java
+++ src/main/java/com/mycompany/tennis/core/entity/Epreuve.java
@@ -1,9 +1,21 @@
 package com.mycompany.tennis.core.entity;
 
+import org.hibernate.annotations.Type;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="EPREUVE")
 public class Epreuve {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Type(type="short")  //permet d'indiquer le type de la variable dans la base de donnée
     private Short annee;
+
+    @Transient //permet d'exclure cette propriété du mapping Hibernate
     private Tournoi tournoi;
+    @Column(name ="TYPE_EPREUVE")
     private Character typeEpreuve;
 
     public Long getId() {
