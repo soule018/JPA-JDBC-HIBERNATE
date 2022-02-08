6bacacfd3331ce8d9986076f92ab7bfc60c2fd5b
diallo <souleymane.diallo@etudiant-univ-rennes1.fr>
2022-02-08 16:56:09 +0100

    Section 7 / Exercice 7 : Utilisez Hibernate pour supprimer un Tournoi

--- src/main/java/com/mycompany/tennis/core/repository/TournoiRepositoryImpl.java
+++ src/main/java/com/mycompany/tennis/core/repository/TournoiRepositoryImpl.java
@@ -2,6 +2,7 @@ package com.mycompany.tennis.core.repository;
 
 import com.mycompany.tennis.core.DataSourceProvider;
 import com.mycompany.tennis.core.HibernateUtil;
+import com.mycompany.tennis.core.entity.Joueur;
 import com.mycompany.tennis.core.entity.Tournoi;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
@@ -14,135 +15,18 @@ import java.util.List;
 public class TournoiRepositoryImpl {
 
     public void create (Tournoi tournoi){
-
-        Session session = null;
-        Transaction tx=null;
-
-        try {
-            // opensession va nous permettre de recupérer une session fraîche
-            session= HibernateUtil.getSessionFactory().openSession();
-            tx= session.beginTransaction();
-
-             /*
-        Comment demander à Hibernate de créer cet enregistrement ?
-        A l'intérieur de la méthode persist(), on inscrit l'objet qu'on veut créer,
-        ici joueur;
-        La méthode persist() signifie que l'objet passé en paramètre doit être ajouté
-        en session hibernate. Le fait de l'ajouer fait que son état est désormais persistent,
-        tout simplement, on dit qu'il est passé de l'état Transient(càd inconu de la session)
-        à l'état persistent(càd dont la gestion de la persistance est confiée à hibernate.
-        Mais l'action de générer la requête en insert vers la base de donnée ne va pas se faire automatiquement
-        à chaque fois qu'on ajoute un objet en session.Il y a plusieurs mécanismes possibles
-        mais de base, on va pouvoir déclencher manuellement l'insertion grâce à un session.fluch
-                 */
+            Session session= HibernateUtil.getSessionFactory().getCurrentSession();
             session.persist(tournoi);
-            /*
-             il s'agit de synchroniser l'état de la session avec la base de donnée, il invoque fluch de façon indirecte;
-             lorsqu'on fait un commit, il faut penser au rollback lorsque tous ne se passe pas bien
-             */
-            tx.commit();
             System.out.println("Tournoi créé");
         }
-        catch (Exception e){
-            if(tx!=null) {
-                tx.rollback();
-            }
-            e.printStackTrace();
-        }
-        finally {
-            if(session!=null){
-                session.close();
-            }
 
-        }
-
-    }
-
-
-    public void update (Tournoi tournoi) {
-
-        Connection conn = null;
-        try {
-
-            DataSource dataSource = DataSourceProvider.getSingleDataSourceInstance();
-            conn = dataSource.getConnection();
-
-            conn = dataSource.getConnection();
-
-            PreparedStatement preparedStatement = conn.prepareStatement("UPDATE TOURNOI SET NOM=?,CODE=? WHERE ID=?");
-            /*
-            les méthodes set de preparedStatement prennent en 1er paramètre
-            l'index du ? dans la requête sql,
-            ici, on a un seul paramètre,
-            le 2ème paramètre est la valeur qu'on veut donner à ce ?
-             */
-
-            preparedStatement.setString(1, tournoi.getNom());
-            preparedStatement.setString(2, tournoi.getCode());
-
-            preparedStatement.setLong(3,tournoi.getId());
-
-            preparedStatement.executeUpdate();
-
-
-            System.out.println("Tournoi modifié");
-        } catch (SQLException e) {
-            e.printStackTrace();
-            try {
-                if (conn != null) conn.rollback();
-            } catch (SQLException e1) {
-                e1.printStackTrace();
-            }
-        } finally {
-            try {
-                if (conn != null) {
-                    conn.close();
-                }
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        }
-    }
 
     public void delete (Long id) {
 
-        Connection conn = null;
-        try {
-
-            DataSource dataSource = DataSourceProvider.getSingleDataSourceInstance();
-            conn = dataSource.getConnection();
-
-            conn = dataSource.getConnection();
-
-            PreparedStatement preparedStatement = conn.prepareStatement("DELETE FROM TOURNOI WHERE ID=?");
-            /*
-            les méthodes set de preparedStatement prennent en 1er paramètre
-            l'index du ? dans la requête sql,
-            ici, on a un seul paramètre,
-            le 2ème paramètre est la valeur qu'on veut donner à ce ?
-             */
-            preparedStatement.setLong(1,id);
-
-            preparedStatement.executeUpdate();
-
-
-            System.out.println("Tournoi supprimé");
-        } catch (SQLException e) {
-            e.printStackTrace();
-            try {
-                if (conn != null) conn.rollback();
-            } catch (SQLException e1) {
-                e1.printStackTrace();
-            }
-        } finally {
-            try {
-                if (conn != null) {
-                    conn.close();
-                }
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        }
+        Tournoi tournoi=getById(id);
+        Session session=HibernateUtil.getSessionFactory().getCurrentSession();
+        session.delete(tournoi);
+        System.out.println("Tournoi supprimé");
     }
 
     /*
@@ -150,35 +34,12 @@ public class TournoiRepositoryImpl {
      */
     public Tournoi getById (Long id) {
 
-        Tournoi tournoi =null;
+        Tournoi tournoi=null;
         Session session = null;
-        try {
-            // opensession va nous permettre de recupérer une session fraîche
-            session= HibernateUtil.getSessionFactory().openSession();
+        session= HibernateUtil.getSessionFactory().getCurrentSession();
+        tournoi=session.get(Tournoi.class,id);
 
-             /*
-             C'est à partir de l'objet session qu'on va pouvoir faire du create,update,delete,read
-             Pour récupérer une ligne particulière dans la base de donnée et en faire
-             un objet,on utlise la méthode get de session,
-             Il prendra en argument d'abord le type d'objet qu'on veut fabriquer et en deuxième argument
-             l'identifiant unique de l'objet en particulier qu'on veut créer, sous-entendu de la ligne
-             qu'on veut recupérer à partir de la table associé à cette table joueur;
-             On aura pas à écrire la requête sql qui lui va bien, on aura pas à récupérer du resulSet,
-             On aura pas à instancier nous-même la classe joueur et à associer à toutes ses propriétés les valeurs
-             issues des colonnes des tables, non, hibernate fait cela pour nous
-              */
-            tournoi=session.get(Tournoi.class,id);
-            System.out.println("Tournoi lu");
-        }
-        catch (Throwable t){
-            t.printStackTrace();
-        }
-        finally {
-            if(session!=null){
-                session.close();
-            }
-
-        }
+        System.out.println("Tournoi lu");
         return tournoi;
     }
 
--- src/main/java/com/mycompany/tennis/core/service/TournoiService.java
+++ src/main/java/com/mycompany/tennis/core/service/TournoiService.java
@@ -1,7 +1,10 @@
 package com.mycompany.tennis.core.service;
 
+import com.mycompany.tennis.core.HibernateUtil;
 import com.mycompany.tennis.core.entity.Tournoi;
 import com.mycompany.tennis.core.repository.TournoiRepositoryImpl;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 
 
 public class TournoiService {
@@ -23,12 +26,157 @@ public class TournoiService {
     on appelle cela un service passe-plat
      */
     public void createTournoi(Tournoi tournoi){
-        tournoiRepository.create(tournoi);
+        Session session=null;
+        Transaction tx=null;
+        try {
+            /*
+            getCurrentSession() va permettre de réutiliser une session qui sera
+            stocker quelque part ici en l'occurence dans le ThreadLocal
+             */
+            session= HibernateUtil.getSessionFactory().getCurrentSession();
+            tx=session.beginTransaction();
+            tournoiRepository.create(tournoi);
+            tx.commit();
+        }
+        catch (Exception e){
+            if(tx!=null) {
+                tx.rollback();
+            }
+            e.printStackTrace();
+        }
+        finally {
+            if(session!=null){
+                session.close();
+            }
 
+        }
     }
 
     public Tournoi getTournoi (Long id) {
-        Tournoi tournoi=tournoiRepository.getById(id);
+        Session session=null;
+        Transaction tx=null;
+        Tournoi tournoi=null;
+        try {
+            /*
+            getCurrentSession() va permettre de réutiliser une session qui sera
+            stocker quelque part ici en l'occurence dans le ThreadLocal
+             */
+            session= HibernateUtil.getSessionFactory().getCurrentSession();
+            tx=session.beginTransaction();
+            tournoi=tournoiRepository.getById(id);
+            tx.commit();
+        }
+        catch (Exception e){
+            if(tx!=null) {
+                tx.rollback();
+            }
+            e.printStackTrace();
+        }
+        finally {
+            if(session!=null){
+                session.close();
+            }
+
+        }
+
         return tournoi;
+    }
+
+    /*
+   Cette méthode permet de renommer un tournoi dejà existant;
+   Pour ce faire, on recupère d'abord le joueur qu'on veut modifier via son id,
+   on utilise donc la méthode getById(), puis on modifie le nom du joueur via un setNom;
+    */
+    public void update(Long id, String nouveauNom){
+        Tournoi tournoi=getTournoi(id); // joueur qui sert de modèle
+        Session session=null;
+        Transaction tx=null;
+        try {
+            /*
+            getCurrentSession() va permettre de réutiliser une session qui sera
+            stocker quelque part ici en l'occurence dans le ThreadLocal
+             */
+            session= HibernateUtil.getSessionFactory().getCurrentSession();
+            tx=session.beginTransaction();
+            tournoi.setNom(nouveauNom);
+            /*
+            La méthode merge() va d'abord procéder à un select des informations qui se trouvent en base de donnée relative au joueur ayant le même id,
+            l'id. Cela va conduire à la création d'un nouvel objet qui sera mis en session, qui est persistant,
+            et enfin la méthode merge va modifier (update) toutes les propriétés de l'objet persistant en utilisant les propriété de l'objet
+            non persistant, ce qui va conduire à la fin à un update,
+            On a donc deux objets joueur à ce stade avec le même id, celui qui a servi de modèle et qui est détaché et celui qui est persistant
+             */
+            Tournoi TournoiPersistant=(Tournoi) session.merge(tournoi); // joueur persistant
+            tx.commit();
+        }
+        catch (Exception e){
+            if(tx!=null) {
+                tx.rollback();
+            }
+            e.printStackTrace();
+        }
+        finally {
+            if(session!=null){
+                session.close();
+            }
+
+        }
+    }
+   /*
+    Cette méthode permet de renommer le code d'un tournoi dejà existant;
+    Pour ce faire, on recupère d'abord le joueur qu'on veut modifier via son id,
+    on utilise donc la méthode getById(), puis on modifie le sexe du joueur via un setSexe;
+    */
+    public void updateByCode(Long id, String code) {
+        Tournoi tournoi = getTournoi(id); // joueur qui sert de modèle
+        Session session = null;
+        Transaction tx = null;
+        try {
+            /*
+            getCurrentSession() va permettre de réutiliser une session qui sera
+            stocker quelque part ici en l'occurence dans le ThreadLocal
+             */
+            session = HibernateUtil.getSessionFactory().getCurrentSession();
+            tx = session.beginTransaction();
+            tournoi.setCode(code);
+            Tournoi tournoiPersistant = (Tournoi) session.merge(tournoi); // joueur persistant
+            tx.commit();
+        } catch (Exception e) {
+            if (tx != null) {
+                tx.rollback();
+            }
+            e.printStackTrace();
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+
+        }
+    }
+
+    public  void deleteTournoi (Long id){
+        Session session = null;
+        Transaction tx = null;
+        try {
+            /*
+            getCurrentSession() va permettre de réutiliser une session qui sera
+            stocker quelque part ici en l'occurence dans le ThreadLocal
+             */
+            session = HibernateUtil.getSessionFactory().getCurrentSession();
+            tx = session.beginTransaction();
+            tournoiRepository.delete(id);
+            tx.commit();
+        } catch (Exception e) {
+            if (tx != null) {
+                tx.rollback();
+            }
+            e.printStackTrace();
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+
+        }
+
     }
 }
